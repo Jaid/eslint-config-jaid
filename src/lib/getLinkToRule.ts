f1d56fbb982ef02c95e8b680b65cb4abf9750383
Jaid <6216144+Jaid@users.noreply.github.com>
2023-08-14 18:43:21 +0200

    Applied ESLint code fixes

--- src/lib/getLinkToRule.ts
+++ src/lib/getLinkToRule.ts
@@ -1,29 +1,27 @@
-type Mapper = {
-  [key: string]: (id: string) => string
-}
+type Mapper = Record<string, (id: string) => string>;
 
 export const pluginMap = {
-  'eslint-plugin-import-quotes': () => 'https://github.com/xneek/eslint-plugin-import-quotes#readme',
-  'eslint-plugin-import': 'https://github.com/import-js/eslint-plugin-import',
+  'eslint-plugin-import-quotes': () => `https://github.com/xneek/eslint-plugin-import-quotes#readme`,
+  'eslint-plugin-import': `https://github.com/import-js/eslint-plugin-import`,
 }
 
 const map: Mapper = {
   eslint: id => `https://eslint.org/docs/latest/rules/${id}`,
-  'eslint-plugin-import': id => `https://github.com/import-js/eslint-plugin-import#import${id.replaceAll('-', '')}`,
+  'eslint-plugin-import': id => `https://github.com/import-js/eslint-plugin-import#import${id.replaceAll(`-`, ``)}`,
   'eslint-plugin-you-dont-need-lodash-underscore': id => `https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_${id}`,
   'eslint-plugin-node': id => `https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/${id}.md`,
   'eslint-plugin-promise': id => `https://github.com/eslint-community/eslint-plugin-promise/blob/main/docs/rules/${id}.md`,
   'eslint-plugin-react': id => `https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/${id}.md`,
   '@typescript-eslint': id => `https://typescript-eslint.io/rules/${id}`,
-  'eslint-plugin-unicorn': id => `https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/${id}.md`
+  'eslint-plugin-unicorn': id => `https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/${id}.md`,
 }
 
 export default (ruleId: string): string | null => {
-  const parts = ruleId.split('/')
+  const parts = ruleId.split(`/`)
   let plugin
   let id
   if (parts.length === 1) {
-    plugin = 'eslint'
+    plugin = `eslint`
     id = parts[0]
   } else {
     plugin = parts[0]
--- src/scripts/unused.ts
+++ src/scripts/unused.ts
@@ -1,34 +1,35 @@
 #!/bin/env tsx
-import execa from 'execa'
-import path from 'path'
+import path from 'node:path'
+
 import eslintFindRules from 'eslint-find-rules'
+import execa from 'execa'
 import * as lodash from 'lodash-es'
 import {firstMatch} from 'super-regex'
 
-await execa('npm', ['run', 'build'])
-const rulesFile = path.join('dist', 'build', 'react', 'index.json')
+await execa(`npm`, [`run`, `build`])
+const rulesFile = path.join(`dist`, `build`, `react`, `index.json`)
 const ruleFinder = await eslintFindRules(rulesFile)
 const unusedRules: string[] = ruleFinder.getUnusedRules().filter(ruleId => {
-  return ruleId.split('/').length <= 2
+  return ruleId.split(`/`).length <= 2
 })
 interface Rule {
   id: string
   plugin?: string
 }
 const rules: Rule[] = unusedRules.map(ruleId => {
-  const parts = ruleId.split('/')
+  const parts = ruleId.split(`/`)
   if (parts.length === 1) {
     return {
       id: ruleId,
-      plugin: 'eslint'
+      plugin: `eslint`,
     }
   }
   return {
     id: parts[1],
-    plugin: parts[0]
+    plugin: parts[0],
   }
 })
-const groupedRules = lodash.groupBy(rules, "plugin")
+const groupedRules = lodash.groupBy(rules, `plugin`)
 for (const [plugin, ruleList] of Object.entries(groupedRules)) {
   console.log(`\n${plugin}:`)
   for (const rule of ruleList) {
