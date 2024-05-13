d52edafe1d6cc8e29ff865d55073fa61818815d3
Jaid <6216144+Jaid@users.noreply.github.com>
2024-05-13 23:53:34 +0200

    Testing
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- test/lib/runTest.ts
+++ test/lib/runTest.ts
@@ -1,8 +1,7 @@
 import type {TestContext} from 'more-types'
 import type * as Main from 'src/index.js'
 
-import stylisticJs from '@stylistic/eslint-plugin-js'
-import {ESLint} from 'eslint'
+import {loadESLint} from 'eslint'
 import * as path from 'forward-slash-path'
 import {getMainModule} from 'zeug'
 
@@ -12,14 +11,15 @@ const main = await getMainModule<typeof Main>(`eslint-config-jaid`)
 export const runTest = async (testContext: TestContext) => {
   const fixtureFolder = path.join(fixturesFolder, testContext.name)
   const srcFolder = path.join(fixtureFolder, `src`)
-  const Eslint = loadEslint
-  const eslint = new ESLint({
-    useEslintrc: false,
-    cache: false,
+  const Eslint = await loadESLint({
+    useFlatConfig: true,
     cwd: fixtureFolder,
-    // baseConfig: [
-    // main.typescriptConfig
-    // ]
+  })
+  const eslint = new Eslint({
+    overrideConfig: [
+      main.typescriptConfig
+    ],
+    baseConfig: {}
   })
   const c = await eslint.calculateConfigForFile(srcFolder + `/a.ts`)
   console.dir({c}, {depth: null})
