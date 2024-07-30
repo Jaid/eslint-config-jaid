157f1a5555ce9b11893adf4064980383e2c1af50
Jaid <6216144+Jaid@users.noreply.github.com>
2024-07-30 13:12:50 +0200

    Extended test
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

---
+++ test/fixture/basic/src/typescript.ts
@@ -0,0 +1,1 @@
+type A  = 1 | 2 | {a: boolean, b:number} | 3
--- test/lib/runTest.ts
+++ test/lib/runTest.ts
@@ -1,14 +1,15 @@
-import {globby} from 'globby'
 import type {Dict, TestContext} from 'more-types'
 import type * as Main from 'src/index.js'
-import {AsyncReturnType, type Entry, type IterableElement} from 'type-fest'
+import type {AsyncReturnType} from 'type-fest'
 
 import {loadESLint} from 'eslint'
 import * as path from 'forward-slash-path'
-import {getMainModule,toCleanYamlFile} from 'zeug'
+import {globby} from 'globby'
+import {type IterableElement} from 'type-fest'
+import {getMainModule, toCleanYamlFile} from 'zeug'
 
 const fixturesFolder = path.join(import.meta.dirname, `..`, `fixture`)
-const outFolder = path.join(import.meta.dirname, `..`, `out`, "test")
+const outFolder = path.join(import.meta.dirname, `..`, `..`, `out`, `test`)
 const main = await getMainModule<typeof Main>(`eslint-config-jaid`)
 
 export const runTest = async (testContext: TestContext) => {
@@ -17,24 +18,22 @@ export const runTest = async (testContext: TestContext) => {
   const Eslint = await loadESLint({
     useFlatConfig: true,
     cwd: fixtureFolder,
-  }) as typeof import('eslint').ESLint
+  }) as typeof import('eslint').ESLint // eslint-disable-line stylistic/quotes
   const eslint = new Eslint({
-    baseConfig: [
-      main.typescriptConfig
-    ],
+    baseConfig: [main.typescriptConfig],
     cwd: fixtureFolder,
   })
-  const tsNames = await globby(["**/*.{js,ts}"], {
+  const tsNames = await globby([`**/*.{js,ts}`], {
     cwd: fixtureFolder,
     onlyFiles: true,
   })
   const calculatedConfig = await eslint.calculateConfigForFile(path.join(srcFolder, tsNames[0]))
-  await toCleanYamlFile(calculatedConfig, path.join(outFolder, "config.yml"))
+  await toCleanYamlFile(calculatedConfig, path.join(outFolder, `config.yml`))
   const results: Dict<IterableElement<AsyncReturnType<typeof eslint.lintFiles>>> = {}
   for (const tsName of tsNames) {
     const scriptFile = path.join(fixtureFolder, tsName)
     const result = await eslint.lintFiles(scriptFile)
     results[tsName] = result[0]
-    await toCleanYamlFile(result, path.join(outFolder, "result", `${tsName}.yml`))
+    await toCleanYamlFile(result, path.join(outFolder, `result`, `${tsName}.yml`))
   }
 }
