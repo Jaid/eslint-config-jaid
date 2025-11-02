68e7af6462759ee70344d5146bb09a20e99162e4
Jaid <6216144+Jaid@users.noreply.github.com>
2025-11-02 01:38:24 +0000

    Added resolveRuleId again
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/lib/unpackRuleset.ts
+++ src/lib/unpackRuleset.ts
@@ -1,12 +1,25 @@
 import type {Linter} from 'eslint'
 import type {Arrayable} from 'type-fest'
 
+import * as lodash from 'lodash-es'
+
 export type Ruleset<RulesGeneric = unknown> = {
   error?: Dict<Arrayable<unknown>>
   id: string
   warn?: Dict<Arrayable<unknown>>
 }
 
-export const unpackRuleset = <RulesGeneric = unknown>(configSet: Ruleset<RulesGeneric>): Linter.Config<RulesGeneric>['rules'] => {
-
+export const unpackRuleset = <RulesGeneric = unknown>(ruleset: Ruleset<RulesGeneric>): Linter.Config<RulesGeneric>['rules'] => {
+  const resolveRuleId = (internalId: string) => {
+    if (internalId.includes('/')) {
+      const [pluginName, ruleName] = internalId.split('/')
+      const kebabName = lodash.kebabCase(ruleName)
+      return `${pluginName}/${kebabName}`
+    }
+    const kebabName = lodash.kebabCase(internalId)
+    if (ruleset.id === 'eslint') {
+      return kebabName
+    }
+    return `${ruleset.id}/${kebabName}`
+  }
 }
