f24dc337bdd5bb0867ee80dff015df0f6e049825
Jaid <6216144+Jaid@users.noreply.github.com>
2025-11-03 09:45:34 +0000

    Improved perfectionist settings

--- src/segments/typescript/rules/perfectionist.ts
+++ src/segments/typescript/rules/perfectionist.ts
@@ -1,164 +1,215 @@
 import type {Ruleset} from 'lib/unpackRuleset.ts'
 
-const objectKeySeries = (...keys: Array<string>) => ({
-  type: 'natural',
-  groups: keys,
-  customGroups: Object.fromEntries(keys.map(key => [key, `^${key}$`])),
-  useConfigurationIf: {
-    allNamesMatchPattern: `^(${keys.join('|')})$`,
-  },
-})
+const objectKeySeries = (...keys: Array<string>) => {
+  return {
+    type: 'natural',
+    groups: keys,
+    customGroups: Object.fromEntries(keys.map(key => [key, `^${key}$`])),
+    useConfigurationIf: {
+      allNamesMatchPattern: `^(${keys.join('|')})$`,
+    },
+  }
+}
 
-export const perfectionistRules = (): Ruleset => ({
-  id: 'perfectionist',
-  warn: {
-    sortClasses: {
-      type: 'natural',
-      groups: [
-        'static-property',
-        'static-method',
-        'static-private-method',
-        'index-signature',
-        'property',
-        'private-property',
-        'constructor',
-        [
-          'get-method',
-          'set-method',
-        ],
-        'method',
-        'private-method',
-        'unknown',
-      ],
-    },
-    sortEnums: {
-      type: 'natural',
-    },
-    sortImports: {
-      type: 'natural',
-      groups: [
-        [
-          'parent-type',
-          'sibling-type',
-          'external-type',
-          'internal-type',
-          'internal-with-shortcut-type',
-          'index-type',
-        ],
-        'side-effect',
-        'object',
-        [
-          'builtin',
-          'node',
-        ],
-        'external',
-        [
-          'internal',
-          'internal-with-shortcut',
-        ],
-        [
-          'parent',
-          'sibling',
-        ],
-        'style',
-        'unknown',
-      ],
-      customGroups: {
-        type: {
-          'internal-with-shortcut-type': 'internal-with-shortcut',
-        },
-        value: {
-          node: '^(bun|node):.+',
-          'internal-with-shortcut': [
-            '^src/.+',
-            '^lib/.+',
-            '^component/.+',
-            '^components/.+',
+export const perfectionistRules = (): Ruleset => {
+  return {
+    id: 'perfectionist',
+    warn: {
+      sortClasses: {
+        type: 'natural',
+        groups: [
+          'static-property',
+          'static-method',
+          'static-private-method',
+          'index-signature',
+          'property',
+          'private-property',
+          'constructor',
+          [
+            'get-method',
+            'set-method',
           ],
+          'method',
+          'private-method',
+          'unknown',
+        ],
+      },
+      sortEnums: {
+        type: 'natural',
+      },
+      sortImports: {
+        type: 'natural',
+        groups: [
+          [
+            'parent-type',
+            'sibling-type',
+            'external-type',
+            'internal-type',
+            'internal-with-shortcut-type',
+            'index-type',
+          ],
+          'side-effect',
+          'object',
+          [
+            'builtin',
+            'node',
+          ],
+          'external',
+          [
+            'internal',
+            'internal-with-shortcut',
+          ],
+          [
+            'parent',
+            'sibling',
+          ],
+          'style',
+          'unknown',
+        ],
+        customGroups: {
+          type: {
+            'internal-with-shortcut-type': 'internal-with-shortcut',
+          },
+          value: {
+            node: '^(bun|node):.+',
+            'internal-with-shortcut': [
+              '^src/.+',
+              '^lib/.+',
+              '^component/.+',
+              '^components/.+',
+            ],
+          },
         },
       },
-    },
-    sortInterfaces: {
-      type: 'natural',
-    },
-    sortNamedExports: {
-      type: 'natural',
-    },
-    sortNamedImports: {
-      type: 'natural',
-    },
-    sortObjectTypes: {
-      type: 'natural',
-    },
-    sortDecorators: {
-      type: 'natural',
-    },
-    sortHeritageClauses: {
-      type: 'natural',
-    },
-    sortUnionTypes: {
-      type: 'natural',
-      groups: [
-        'unknown',
-        'literal',
-        'keyword',
-        'nullish',
-      ],
-    },
-    sortObjects: [
-      objectKeySeries('r', 'g', 'b'),
-      objectKeySeries('x', 'y', 'z'),
-      objectKeySeries('width', 'height'),
-      objectKeySeries('id', 'identifier', 'key', 'uuid', 'type', 'name', 'title', 'value', 'description'),
-      objectKeySeries('createdAt', 'updatedAt', 'deletedAt'),
-      objectKeySeries('before', 'after'),
-      objectKeySeries('from', 'to'),
-      objectKeySeries('min', 'minimum', 'max', 'maximum'),
-      objectKeySeries('in', 'input', 'out', 'output'),
-      objectKeySeries('top', 'bottom', 'left', 'right'),
-      {
-        type: 'unsorted',
+      sortInterfaces: {
+        type: 'natural',
       },
-    ],
-    sortModules: {
-      customGroups: [
+      sortNamedExports: {
+        type: 'natural',
+      },
+      sortNamedImports: {
+        type: 'natural',
+      },
+      sortObjectTypes: {
+        type: 'natural',
+      },
+      sortDecorators: {
+        type: 'natural',
+      },
+      sortHeritageClauses: {
+        type: 'natural',
+      },
+      sortUnionTypes: {
+        type: 'natural',
+        groups: [
+          'unknown',
+          'literal',
+          'keyword',
+          'nullish',
+        ],
+      },
+      sortObjects: [
+        objectKeySeries('r', 'g', 'b'),
+        objectKeySeries('x', 'y', 'z'),
+        objectKeySeries('width', 'height'),
+        objectKeySeries('id', 'identifier', 'key', 'uuid', 'type', 'name', 'title', 'value', 'description'),
+        objectKeySeries('createdAt', 'updatedAt', 'deletedAt'),
+        objectKeySeries('before', 'after'),
+        objectKeySeries('from', 'to'),
+        objectKeySeries('min', 'minimum', 'max', 'maximum'),
+        objectKeySeries('in', 'input', 'out', 'output'),
+        objectKeySeries('top', 'bottom', 'left', 'right'),
         {
-          groupName: 'typescript',
-          anyOf: [
-            {
-              selector: 'interface',
-            },
-            {
-              selector: 'type',
-            },
-            {
-              selector: 'enum',
-            },
-          ],
-          type: 'unsorted',
-        },
-        {
-          groupName: 'functionImplementations',
-          anyOf: [
-            {
-              selector: 'function',
-            },
-          ],
-          type: 'unsorted',
-        },
-        {
-          groupName: 'classImplementations',
-          anyOf: [
-            {
-              selector: 'class',
-            },
-          ],
           type: 'unsorted',
         },
       ],
+      sortModules: {
+        groups: [
+          'exportedTypes',
+          'types',
+          'exportedFunctionImplementations',
+          'functionImplementations',
+          'exportedClassImplementations',
+          'classImplementations',
+          'unknown',
+        ],
+        customGroups: [
+          {
+            groupName: 'exportedTypes',
+            anyOf: [
+              {
+                selector: 'interface',
+                modifiers: ['export'],
+              },
+              {
+                selector: 'type',
+                modifiers: ['export'],
+              },
+              {
+                selector: 'enum',
+                modifiers: ['export'],
+              },
+            ],
+            type: 'unsorted',
+          },
+          {
+            groupName: 'types',
+            anyOf: [
+              {
+                selector: 'interface',
+              },
+              {
+                selector: 'type',
+              },
+              {
+                selector: 'enum',
+              },
+            ],
+            type: 'unsorted',
+          },
+          {
+            groupName: 'exportedFunctionImplementations',
+            anyOf: [
+              {
+                selector: 'function',
+                modifiers: ['export'],
+              },
+            ],
+            type: 'unsorted',
+          },
+          {
+            groupName: 'functionImplementations',
+            anyOf: [
+              {
+                selector: 'function',
+              },
+            ],
+            type: 'unsorted',
+          },
+          {
+            groupName: 'exportedClassImplementations',
+            anyOf: [
+              {
+                selector: 'class',
+                modifiers: ['export'],
+              },
+            ],
+            type: 'unsorted',
+          },
+          {
+            groupName: 'classImplementations',
+            anyOf: [
+              {
+                selector: 'class',
+              },
+            ],
+            type: 'unsorted',
+          },
+        ],
+      },
+      sortExports: {
+        type: 'natural',
+      },
     },
-    sortExports: {
-      type: 'natural',
-    },
-  },
-})
+  }
+}
