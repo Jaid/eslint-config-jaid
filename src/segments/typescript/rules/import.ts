f0e562e87d7e998c3e485f211cd234ec5d94f71b
Jaid <6216144+Jaid@users.noreply.github.com>
2026-02-21 19:18:25 +0000

    Disabled import/extensions rule
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/segments/typescript/rules/import.ts
+++ src/segments/typescript/rules/import.ts
@@ -10,12 +10,15 @@ export const importRules = (): Ruleset => ({
     noDuplicates: [],
     noMutableExports: [],
     noUselessPathSegments: [],
-    extensions: [
-      "ignorePackages",
-      {
-        fix: true, // We currently have to manually enable fixing, this will be default in v5: https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/extensions.md
-      },
-    ],
+     // WAITINGFOR https://github.com/un-ts/eslint-plugin-import-x/issues/437
+     // WAITINGFOR https://github.com/un-ts/eslint-plugin-import-x/issues/413
+     // WAITINGFOR v5 – We currently have to manually enable fixing, this will be default in v5: https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/extensions.md
+    // extensions: [
+    //   "ignorePackages",
+    //   {
+    //     fix: true,
+    //   },
+    // ],
     consistentTypeSpecifierStyle: [],
   },
   error: {
