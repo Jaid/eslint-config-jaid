d13205b4c34e772c85dcda615ffd26bb541a2d5d
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-14 19:53:13 +0100

    chore: Migrated from “node:path” to “forward-slash-path”

--- test/fixture/clean-ts/src/with-imports.ts
+++ test/fixture/clean-ts/src/with-imports.ts
@@ -1,5 +1,5 @@
 import * as nodeOs from 'node:os'
-import * as nodePath from 'node:path'
+import * as path from 'forward-slash-path'
 
 const dataFolder = nodePath.join(nodeOs.homedir(), 'data')
 export {dataFolder}
--- test/fixture/violations-ts/src/unsorted-imports.ts
+++ test/fixture/violations-ts/src/unsorted-imports.ts
@@ -1,4 +1,4 @@
-import * as nodePath from 'node:path'
+import * as path from 'forward-slash-path'
 import * as nodeOs from 'node:fs'
 
 const result = nodePath.join('a', 'b')
