29826f46f024823b0fb905c7feb9e621d42f8cb4
Jaid <6216144+Jaid@users.noreply.github.com>
2026-09-14 21:27:56 +0200

    build: Generate declarations with Rolldown

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,11 +1,11 @@
 import type {Plugin} from 'rolldown'
 import type {PackageJson} from 'type-fest'
 
-import {generateDtsBundle} from 'dts-bundle-generator'
 import * as path from 'forward-slash-path'
 import fs from 'fs-extra'
 import publishimo from 'publishimo'
 import {defineConfig} from 'rolldown'
+import {dts} from 'rolldown-plugin-dts'
 
 const rootFolder = import.meta.dirname
 const sourceFile = path.join(rootFolder, 'src/index.ts')
@@ -22,20 +22,6 @@ const packagePlugin = (): Plugin => {
   return {
     name: 'eslint-config-jaid-package',
     async writeBundle() {
-      if (isProduction) {
-        const [declaration] = generateDtsBundle([
-          {
-            filePath: sourceFile,
-            output: {
-              exportReferencedTypes: false,
-              noBanner: true,
-              sortNodes: true,
-            },
-          },
-        ])
-        await fs.outputFile(path.join(outputFolder, outputTypes), declaration)
-      }
-
       const publishimoResult = await publishimo({
         fetchGithub: false,
         pkg: packageJson,
@@ -80,5 +66,8 @@ export default defineConfig({
     format: 'esm',
     sourcemap: 'hidden',
   },
-  plugins: [packagePlugin()],
+  plugins: [
+    ...(isProduction ? [dts({generator: 'tsc'})] : []),
+    packagePlugin(),
+  ],
 })
